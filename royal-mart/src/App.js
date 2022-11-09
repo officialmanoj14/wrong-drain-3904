3c29aa7543dd4fa437fe833841e531a27d7f2f0b
Manoj Kumar <officailmanoj14@gmail.com>
2022-11-09 19:28:07 +0530

    LandingPage work is ongoing

---
+++ royal-mart/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
